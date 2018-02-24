51c0803542a7ee536c8add41aa710e3f8dd0520f
task650 <task650@gmail.com>
2018-02-23 16:40:50 -0800

    Squash/Merge tag 'lineage-15.1' into oreo
    * 72 open commits for one repo is crazy to
    leave open. Lets squash it. ;)
    
    Change-Id: I4f77517967c10421f8a9e0238c5096179aa87705

--- Android.mk
+++ Android.mk
@@ -5,29 +5,29 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PACKAGE_NAME := CyanogenSetupWizard
+LOCAL_PACKAGE_NAME := LineageSetupWizard
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_OVERRIDES_PACKAGES := Provision
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
-
-LOCAL_JAVA_LIBRARIES := telephony-common
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4 \
     android-support-v13 \
-    play \
     libphonenumber \
-    org.cyanogenmod.platform.sdk
+    org.lineageos.platform.internal \
+    org.lineageos.platform.sdk
 
-# Include res dir from chips
-google_play_dir := ../../../external/google/google_play_services/libproject/google-play-services_lib/res
-res_dir := $(google_play_dir) res
+LOCAL_JAVA_LIBRARIES := \
+    telephony-common
 
-LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dir))
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_AAPT_FLAGS := --auto-add-overlay
-LOCAL_AAPT_FLAGS += --extra-packages com.google.android.gms
 
+include frameworks/opt/setupwizard/navigationbar/common.mk
 include frameworks/opt/setupwizard/library/common.mk
 
 include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
--- tests/Android.mk
+++ tests/Android.mk
@@ -6,12 +6,12 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.cyanogenmod.platform.sdk
+    org.lineageos.platform.sdk
 
 # Include all test java files.
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := CyanogenSetupWizardTests
+LOCAL_PACKAGE_NAME := LineageSetupWizardTests
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
